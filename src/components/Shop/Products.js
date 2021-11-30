727429bec49c2bbd13476b34fa39a15721ae3e4b
shinshilkina <anastasiashilina98@gmail.com>
2021-11-30 16:43:29 +0700

    Refresher / Practice

--- src/components/Shop/Products.js
+++ src/components/Shop/Products.js
@@ -1,16 +1,35 @@
-import ProductItem from './ProductItem';
-import classes from './Products.module.css';
+import ProductItem from "./ProductItem";
+import classes from "./Products.module.css";
+
+const DUMMY_PRODUCTS = [
+  {
+    id: 1,
+    price: 6,
+    name: "Test",
+    description: "This is a first product - amazing!",
+  },
+  {
+    id: 2,
+    price: 4,
+    name: "Test 2",
+    description: "This is a second product - WOW!",
+  },
+];
 
 const Products = (props) => {
   return (
     <section className={classes.products}>
       <h2>Buy your favorite products</h2>
       <ul>
-        <ProductItem
-          title='Test'
-          price={6}
-          description='This is a first product - amazing!'
-        />
+        {DUMMY_PRODUCTS.map((product) => (
+          <ProductItem
+            key={product.id}
+            id={product.id}
+            title={product.name}
+            price={product.price}
+            description={product.description}
+          />
+        ))}
       </ul>
     </section>
   );
